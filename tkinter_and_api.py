a8d493127d314098544b387e53e599ff39d683a3
Andy Weng <andyweng257@gmail.com>
2025-11-10 10:16:10 -0500

    Update tkinter_and_api.py

--- tkinter_and_api.py
+++ tkinter_and_api.py
@@ -1,9 +1,15 @@
 from tkinter import *
-rank = [{"Rank":"loser", "buffs": 1},
+import random
+
+
+
+""" rank = [{"Rank":"loser", "buffs": 1},
         {"Rank":"geek", "buffs": 3},
         {"Rank":"normal", "buffs": 5},
         {"Rank":"cool", "buffs": 8},
-        {"Rank":"amazing", "buffs": 11}]
+        {"Rank":"amazing", "buffs": 11},
+        {"Rank":"Peak", "buffs": 15}]
+
 
 window = Tk()
 window.geometry("600x600")
@@ -11,12 +17,12 @@ window.title("Clicker Game!")
 
 
 number = 0 
-level = 4
+level = 0
 rebirth = rank[level]["buffs"]
 multiplyer = 50*rebirth
 def clicker():
     global number
-    number += 1 * multiplyer * rank[level]["buffs"]
+    number += 1 * multiplyer
     infoshower.config(text = number)
 def upgrade():
     global number
@@ -26,7 +32,6 @@ def upgrade():
         number = 0
     
     multiplyershower.config(text=multiplyer)
-
 def rankup():
     def maxlevel():
         print("you are max rank goodjob")
@@ -38,16 +43,16 @@ def rankup():
         if level == len(rank)-1:
             maxlevel()
         else:
-            multiplyer = 1
-            number = 0
             level +=1
+            multiplyer = 1 * rank[level]["buffs"]
+            number = 0
             rankupbutton.config(text=rank[level]["Rank"])
             multiplyershower.config(text=multiplyer)
+            infoshower.config(text = number)
 
 
 
 
-    
 #clicker
 clickingbutton = Button(window,text="click!", padx=50, pady=50, bg="gold", font=("Arial, 22"), command=clicker)
 
@@ -68,7 +73,7 @@ multiplyershower.pack()
 clickingbutton.pack()
 upgradebutton.pack()
 rankupbutton.pack()
-window.mainloop()
+window.mainloop() """
 
 
 
